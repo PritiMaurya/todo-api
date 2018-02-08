bb1effc78322adeb2d94ece631cbd8492b0370e4
priti maurya <lanetteam.priti@gmail.com>
2018-02-08 17:06:34 +0530

    modify for heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var {ObjectID} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose-coonect')
 var {Todo} = require('./model/todo')
 var {User} = require('./model/user')
 
 var app = new express();
+var port = process.env.PORT || 3001
 app.use(bodyParser.json())
 
 app.post('/todos',(req,res)=>{
@@ -31,7 +33,19 @@ app.get('/todos',(req,res)=>
    })
 });
 
-app.listen(3001,()=>
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.find({_id:id}).then((data)=>{
+        res.send({data});
+    })
+})
+
+
+app.listen(port,()=>
 {
     console.log('started on port 3001');
 });
@@ -40,6 +54,7 @@ module.exports =
     {
         app
     }
+
 
 
 
